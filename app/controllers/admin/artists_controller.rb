13e3841d3c612bb52919d004783ab6dc4ba02770
koya-yoshitomi <koya.yoshitomi@gmail.com>
2018-09-27 10:19:08 +0900

    update gemfile and etc

--- app/controllers/admin/artists_controller.rb
+++ app/controllers/admin/artists_controller.rb
@@ -1,19 +1,42 @@
 class Admin::ArtistsController < Admin::Base
   def index
+    @artist = Artist.new
+    @artists = Artist.all
   end
 
   def show
   end
 
   def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      redirect_to admin_artists_path
+    else
+      render :index
+    end
   end
 
   def edit
+    @artist = Artist.find(params[:id])
   end
 
   def update
+    @artist = Artist.find(params[:id])
+    if @artist.update(artist_params)
+      redirect_to admin_artists_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    artist = Artist.find(params[:id])
+    artist.destroy
+    redirect_to admin_artists_path
   end
+
+  private
+    def artist_params
+      params.require(:artist).permit(:name, :name_kana, :image)
+    end
 end
--- app/controllers/admin/labels_controller.rb
+++ app/controllers/admin/labels_controller.rb
@@ -1,19 +1,42 @@
 class Admin::LabelsController < Admin::Base
   def index
+    @label = Label.new
+    @labels = Label.all
   end
 
   def show
   end
 
   def create
+    @label = Label.new(label_params)
+    if @label.save
+      redirect_to admin_labels_path
+    else
+      render :index
+    end
   end
 
   def edit
+    @label = Label.find(params[:id])
   end
 
   def update
+    @label = Label.find(params[:id])
+    if @label.update(label_params)
+      redirect_to admin_labels_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    label = Label.find(params[:id])
+    label.destroy
+    redirect_to admin_labels_path
   end
+
+  private
+    def label_params
+      params.require(:label).permit(:name)
+    end
 end
