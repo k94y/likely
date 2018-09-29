e0daed89901cdb580c2eabd503450b573dca7342
Tatsuki <watanabe.tatsuki.infratop@gmail.com>
2018-09-29 07:44:35 +0000

    Create a result page

--- app/controllers/user/searches_controller.rb
+++ app/controllers/user/searches_controller.rb
@@ -1,21 +1,22 @@
 class User::SearchesController < User::Base
   def index
-  	sel = params[:sel]
-  	if sel == "1"
+  	@sel = params[:sel].to_i
+  	if @sel == 1
   		#アーティスト
-  		@res = Artist.where('name LIKE ?', "%#{params[:keyword]}%").select("name")
-  	elsif sel == 2
+  		@res = Artist.where('name LIKE ?', "%#{params[:keyword]}%").select("id","name","name_kana")
+  	elsif @sel == 2
   		#曲名
-   		@res = Song.where('name LIKE ?', "%#{params[:keyword]}%").select("name")
-  	elsif sel == 3
+   		@res = Song.where('name LIKE ?', "%#{params[:keyword]}%").select("id","name")
+  	elsif @sel == 3
   		#商品名
-  		@res = Product.where('name LIKE ?', "%#{params[:keyword]}%")
+  		@res = Product.where('name LIKE ?', "%#{params[:keyword]}%").select("id","name")
   	else
   		redirect_to root_path
   	end
   end
 
   def create
+  	#パラメータからクエリパラメータを含むurlを生成する。
   	arg = {sel: params[:sel], keyword: params[:keyword]}
 	redirect_to "/searches?#{arg.to_query}"
   end
