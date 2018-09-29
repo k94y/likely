a7a35ad8ae567a591457be77652818f7f463ce91
Tatsuki <watanabe.tatsuki.infratop@gmail.com>
2018-09-29 06:54:42 +0000

    Created a search page and add a controller

--- app/controllers/user/searches_controller.rb
+++ app/controllers/user/searches_controller.rb
@@ -1,4 +1,22 @@
 class User::SearchesController < User::Base
   def index
+  	sel = params[:sel]
+  	if sel == "1"
+  		#アーティスト
+  		@res = Artist.where('name LIKE ?', "%#{params[:keyword]}%").select("name")
+  	elsif sel == 2
+  		#曲名
+   		@res = Song.where('name LIKE ?', "%#{params[:keyword]}%").select("name")
+  	elsif sel == 3
+  		#商品名
+  		@res = Product.where('name LIKE ?', "%#{params[:keyword]}%")
+  	else
+  		redirect_to root_path
+  	end
+  end
+
+  def create
+  	arg = {sel: params[:sel], keyword: params[:keyword]}
+	redirect_to "/searches?#{arg.to_query}"
   end
 end
