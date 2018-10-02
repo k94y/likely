43e914095a0a2d802ac56659c446b90ad31faeef
TairaOuchi <taira.o19@gmail.com>
2018-10-02 16:16:30 +0900

    [ADD] 注文確定ページ(cart_detailsからorder生成, 生成後はcurrent_userに紐付いたcart_detailsを全て削除)

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,6 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :order_details
 
-  validates :shipment_status, presence: true, numericality: {greater_than_or_equal_to: 0}
-  validates :cancel_request, presence: true
+  # validates :shipment_status, presence: true, numericality: {greater_than_or_equal_to: 0}
+  # validates :cancel_request, presence: true
 end
