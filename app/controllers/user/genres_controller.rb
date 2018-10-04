5a81085550d78c1b6b6a882b9ed2a0ec10887fc6
koya-yoshitomi <koya.yoshitomi@gmail.com>
2018-10-04 16:08:33 +0900

    [update] genres#show kaminari実装

--- app/controllers/user/genres_controller.rb
+++ app/controllers/user/genres_controller.rb
@@ -7,8 +7,8 @@ class User::GenresController < User::Base
   	@genre = Genre.find(params[:id])
 
   	array = []
-  	@products = Product.where(genre_id: params[:id])
-	@products.each do |product|
+  	products = Product.where(genre_id: params[:id])
+	products.each do |product|
 		product.discs.each do |disc|
 			disc.track_relations.each do |t|
 				array.push({
@@ -20,7 +20,7 @@ class User::GenresController < User::Base
 			end
 		end
 	end
-	@songs = Kaminari.paginate_array(array).page(params[:page]).per(4)
+	@songs = Kaminari.paginate_array(array).page(params[:page]).per(10)
 	# per(x)でページごとの表示件数をxに決定
 
   end
