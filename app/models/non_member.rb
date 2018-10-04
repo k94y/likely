afad1b43d92c26bd4ca239bd7200337d06b32de1
新井　源己 <araigenki@araiminamotoonorenoMacBook-Pro.local>
2018-10-04 20:49:23 +0900

    create-user-contact

--- app/models/non_member.rb
+++ app/models/non_member.rb
@@ -2,6 +2,6 @@ class NonMember < ApplicationRecord
 	validates :name, presence: true, length: {in: 1..255}
 	validates :email, presence: true, length: {in: 1..255}
 
- 	has_many :contacts #うまくいかないかも
+ 	# has_many :contacts #うまくいかないかも
 
 end
