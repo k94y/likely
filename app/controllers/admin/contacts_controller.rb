4e5e047fbfd48b7b8a7011aba6622abb22cc8d12
Tatsuki <watanabe.tatsuki.infratop@gmail.com>
2018-10-06 07:12:58 +0000

    Created a contact index page.

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -1,17 +1,21 @@
 class Admin::ContactsController < Admin::Base
   def index
   	@contacts = Contact.all.order("id desc")
+  	@no_members = NonMember.all
   end
 
   def show
   	@contact = Contact.find(params[:id])
   	if @contact.user_id.blank?
-  		@user = NonMember.find(@contact.non_member_id)
+  		@name = NonMember.find(@contact.non_member_id).name
   	else
-  		@user = User.find(@contact.user_id)
+  		@name = User.find(@contact.user_id).name
   	end
   end
 
   def update
+  	contact = Contact.find(params[:id])
+  	contact.update(reply_status: true)
+  	redirect_to admin_contacts_path
   end
 end
