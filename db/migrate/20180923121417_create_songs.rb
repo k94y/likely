a881d880a220d94544aae4eb83fd58e31a1243ae
新井　源己 <araigenki@araiminamotoonorenoMacBook-Pro.local>
2018-10-06 15:21:22 +0900

    add-recommend-level

--- db/migrate/20180923121417_create_songs.rb
+++ db/migrate/20180923121417_create_songs.rb
@@ -3,6 +3,7 @@ class CreateSongs < ActiveRecord::Migration[5.2]
     create_table :songs do |t|
       t.string :name
       t.text :file
+      t.integer :recommend_level
 
       t.timestamps
     end
