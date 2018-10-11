1490a927d5a7573bd2aaecab75691d0def743015
koya-yoshitomi <koya.yoshitomi@gmail.com>
2018-10-11 20:34:35 +0900

    fix

--- db/migrate/20181002122844_add_recommend_level_to_song.rb
+++
@@ -1,5 +0,0 @@
-class AddRecommendLevelToSong < ActiveRecord::Migration[5.2]
-  def change
-    add_column :songs, :recommend_level, :integer, default: 0
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_122844) do
+ActiveRecord::Schema.define(version: 2018_09_25_103713) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -31,15 +31,13 @@ ActiveRecord::Schema.define(version: 2018_10_02_122844) do
   end
 
   create_table "contacts", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "non_member_id"
+    t.string "user"
+    t.string "non_member"
     t.string "title"
     t.text "body"
     t.boolean "reply_status", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["non_member_id"], name: "index_contacts_on_non_member_id"
-    t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
   create_table "discs", force: :cascade do |t|
