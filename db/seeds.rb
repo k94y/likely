ce59967502da838faa794abbf2268916084bafe4
TairaOuchi <taira.o19@gmail.com>
2018-10-15 22:44:55 +0900

    no message

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create(email: "admin@admin", name: "admin", name_kana: "アドミン", admin: true, password: "111111")
